448d741593e9c072c56a7ee2b03f8ccc0a414103
Tim Kuntze <timkuntze@Tims-MacBook-Pro.local>
2021-03-02 23:10:27 +0100

    added menue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
 
-  images = ['coding.jpg', 'laptop.jpeg', 'cv_picture.jpg'];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-      setTimeout(() => {
-        this.showImage = true; 
-      }, 10)
-    }, 8000);
-  }
+  
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { MenueComponent } from './menue/menue.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowComponent,
+    MenueComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
